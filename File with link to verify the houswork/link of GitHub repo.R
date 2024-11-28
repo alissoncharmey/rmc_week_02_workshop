2dbba11d67e89540b628bb81158a16adf6d27837
alissoncharmey <alisson.charmey@kispi.uzh.ch>
2024-11-28 15:00:37 +0100

    Link for GitHub repo

---
+++ File with link to verify the houswork/link of GitHub repo.R
@@ -0,0 +1,5 @@
+#Dear GraphTeam, 
+
+#Here is the link to fork my repo. 
+#Because I am late in the homework, I couldnt add collaborators but I understood how I have to do in case I need it. 
+#link: https://github.com/alissoncharmey/rmc_week_02_workshop/blob/main/rmd/daly_report_PARENT.md
